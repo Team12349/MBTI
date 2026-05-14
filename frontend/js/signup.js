5c34adc82980f885041309c9ebb9143983b27c13
Mohamed <mohamedsaid0573@gmail.com>
2026-05-15 01:17:33 +0300

    removed font awesome

--- frontend/js/signup.js
+++ frontend/js/signup.js
@@ -3,17 +3,50 @@ const nameInput = document.getElementById("name");
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 
-if (form) {
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
 
-    let isValid = validateInputs();
+  let isValid = validateInputs();
+  if (!isValid) return;
 
-    if (isValid) {
-      window.location.href = "index.html";
+  const el = document.querySelector("#submitBtn");
+  if (!el) return;
+
+  let message = "";
+  try {
+    const res = await fetch("http://localhost:3000/signup", {
+      method: "POST",
+      body: JSON.stringify({
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!res.ok) {
+      message = "Failed to submit feedback.";
+    } else {
+      message = "Thank you for your feedback!";
+      form.reset();
     }
-  });
-}
+  } catch (err) {
+    console.error("Error submitting form", err);
+    message = "Server connection failed.";
+  }
+
+  el.textContent = message;
+  el.disabled = true;
+  el.style.cursor = "not-allowed";
+
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  el.style.cursor = "pointer";
+  el.disabled = false;
+  el.textContent = "Submit";
+});
 
 function validateInputs() {
   let valid = true;
